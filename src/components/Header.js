47f91f1c8eeec6ab27fb41150098e75dde0fb3ee
Noman Ahmed <contact@noman.dev>
2019-08-24 07:17:12 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Header = (props) => (
     <header id="header" className="alt">
         <h1>Noman Ahmed</h1>
+        <p>&#123; Front End React Developer &#125;</p>
         <p>Coming Soon</p>
     </header>
 )
