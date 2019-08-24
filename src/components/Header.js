bb802a697d94e2cc931334522aa647be2a544855
Noman Ahmed <contact@noman.dev>
2019-08-24 07:18:56 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = (props) => (
     <header id="header" className="alt">
         <h1>Noman Ahmed</h1>
         <p>&#123; Front End React Developer &#125;</p>
-        <p>Coming Soon</p>
+        <p>Coming Soon.</p>
     </header>
 )
 
